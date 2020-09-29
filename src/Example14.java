fabc509e30b2b423570257f4e8b07cb626746462
ismaj7 <71702877+ismaj7@users.noreply.github.com>
2020-09-29 11:28:15 +0200

    Initial commit

---
+++ src/Example14.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Example14 {
+    public static void main (String[] argv) {
+
+        float a;
+        float b;
+        float c;
+        float fst = 0;
+        float snd = 0;
+        float trd = 0;
+
+        Scanner inputValue;
+        inputValue = new Scanner (System.in);
+
+        System.out.println ("Please, insert the first value:");
+        a = inputValue.nextFloat();
+
+        System.out.println ("Now, please insert the second value:");
+        b = inputValue.nextFloat();
+
+        System.out.println ("Last, please insert the third value:");
+        c = inputValue.nextFloat();
+
+        if (a < b && a < c) {
+            fst = a;
+        }else {
+            if (a > b && a > c) {
+                trd = a;
+            }else
+                snd = a;
+        }
+
+        if (b < a && b < c) {
+            fst = b;
+        }else {
+            if (b > a && b > c) {
+                trd = b;
+            }else
+                snd = b;
+        }
+
+        if (c < a && c < b) {
+            fst = c;
+        }else {
+            if (c > a && c > b) {
+                trd = c;
+            }else
+                snd = c;
+        }
+
+        System.out.println ("These numbers set in ascendent order: " + fst + " , " + snd + " , " + trd);
+
+    }
+}
